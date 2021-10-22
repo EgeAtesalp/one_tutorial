53a8d6c1a3afbd46ad4ab76bc1e4adf5936fd457
EgeAtesalp <egeatesalp@gmail.com>
2021-10-22 13:43:27 +0200

    Steps4,5

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,61 @@
 
 use std::collections::HashMap;
+use serde::{Serialize, Deserialize};
+use clap::{Arg, App};
 
 #[macro_use]
 extern crate dotenv_codegen;
 
 use dotenv::dotenv;
-use std::env;
+
+
+
+#[derive(Serialize,Deserialize,Debug)]
+struct Currency{
+    name: String,
+    symbol: String,
+    quote: Quotes,
+}
+
+#[derive(Serialize,Deserialize,Debug)]
+struct Quote{
+    price: f64,
+    percent_change_24h: f64,
+    percent_change_7d: f64,
+}
+
+#[derive(Serialize,Deserialize,Debug)]
+struct Response{ data : HashMap<String,Currency>}
+
+#[derive(Serialize,Deserialize,Debug)]
+struct Quotes(HashMap<String, Quote>);
 
 
 
 
 #[tokio::main]
 async fn main()-> Result<(), Box<dyn std::error::Error>> {
+
+
+
+        let matches = App::new("My Super Program")
+            .version("1.0")
+            .author("Ege A. <egeatesalp@gmail.com>")
+            .about("My implementation of one_tutorial")
+            .arg(Arg::new("currency_list")
+                .about("Pass the currencies you want to query")
+                .min_values(1)
+                .required(true))
+            .get_matches();
+        
+        let currencies = matches.value_of("currency_list").expect("No currencies were being passed");  
+        
     
     let mut params = HashMap::new();
-    params.insert("symbol", "BTC");
+    params.insert("symbol", currencies.to_string());
     dotenv().ok();
+
+
 
     
    
@@ -27,9 +67,11 @@ async fn main()-> Result<(), Box<dyn std::error::Error>> {
         .send()
         .await?;
 
-    let resp = resp.text().await?;
+    let resp = resp.json::<Response>().await?;
 
-     println!("{:#?}", resp);
+    println!("{:#?}", resp);
+
+    
 
     Ok(())
 
